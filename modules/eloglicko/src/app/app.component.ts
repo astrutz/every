5ceab773a5844d04631fd7a0b2cc5b1b5fd74cf4
Alexander Strutz <a.strutz@babiel.com>
2023-09-18 16:42:19 +0200

    refactor: ♻️ Remove unneeded parts from eloglicko

--- modules/eloglicko/src/app/app.component.ts
+++ modules/eloglicko/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'every-root',
+  selector: 'eloglicko-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- modules/eloglicko/src/app/app.routes.ts
+++ modules/eloglicko/src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-export const EloglickoRouting = RouterModule.forChild([
+export const Routes: Route[] = [
   {
     path: 'eloglicko',
     component: AppComponent,
   },
-]);
+];
+
+export const EloglickoRouting = RouterModule.forChild(Routes);
