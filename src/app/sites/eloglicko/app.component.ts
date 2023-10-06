cb9c575f0be077486c59cc6bbb16f381170799b2
Alexander Strutz <a.strutz@babiel.com>
2023-10-06 12:33:26 +0200

    refactor: ♻️ Remove nx and use ng only, closes #8

--- src/app/sites/eloglicko/app.component.ts
+++ src/app/sites/eloglicko/app.component.ts
@@ -7,8 +7,7 @@ import { ParticipantsComponent } from './components/participant/participants.com
   imports: [RouterModule, AppComponent, ParticipantsComponent],
   selector: 'eloglicko-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'modules-eloglicko';
+  title = 'eloglicko';
 }
