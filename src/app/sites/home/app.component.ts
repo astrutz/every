e08bf3c9be0b09517dd10d5aa750c9f70a02bc6c
astrutz <alex-strutz@t-online.de>
2024-04-02 11:24:54 +0200

    refactor: ♻️ Move hero component

--- src/app/sites/home/app.component.ts
+++ src/app/sites/home/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeroComponent],
+  imports: [RouterModule, HeroComponent, AboutComponent],
   selector: 'home-root',
   templateUrl: './app.component.html',
 })
