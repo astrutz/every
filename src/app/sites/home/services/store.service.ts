a06c807ebaa4f64bf3e4a8b98a39f6c544ff3eb7
astrutz <alex-strutz@t-online.de>
2024-04-02 15:29:18 +0200

    refactor: ♻️ Improve image quality

--- src/app/sites/home/services/store.service.ts
+++ src/app/sites/home/services/store.service.ts
@@ -92,7 +92,7 @@ const works: Work[] = [
     description:
       'A modern platform for searching and reading digital publications of the German Federal Bank. This page includes a reactive read progress indication, as well as multiple navigations and an excellent accessibility and performance.',
     href: 'https://www.publikationen.bundesbank.de',
-    preview: '/assets/works/digipub.png',
+    preview: '/assets/works/digipub.webp',
     technologies: ['Angular', 'todo'],
   },
   // todo: more
