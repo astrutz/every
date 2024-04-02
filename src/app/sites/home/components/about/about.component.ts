a1af39bb6c6682f8e02c41362066859ff233646d
astrutz <alex-strutz@t-online.de>
2024-04-02 11:25:17 +0200

    feat: ✨ Add about component

---
+++ src/app/sites/home/components/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'every-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+})
+export class AboutComponent {}
