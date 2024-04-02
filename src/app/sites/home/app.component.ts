61df508d4b9191d971b6c6c245c127a39599d535
astrutz <alex-strutz@t-online.de>
2024-04-02 12:26:10 +0200

    feat: ✨ Add experience component

--- src/app/sites/home/app.component.ts
+++ src/app/sites/home/app.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeroComponent, AboutComponent, SkillsComponent],
+  imports: [RouterModule, HeroComponent, AboutComponent, SkillsComponent, ExperienceComponent],
   selector: 'home-root',
   templateUrl: './app.component.html',
 })
