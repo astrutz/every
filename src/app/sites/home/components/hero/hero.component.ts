f903fbfa4d48f5277336059b97819d8eb7019355
astrutz <alex-strutz@t-online.de>
2024-04-08 15:49:36 +0200

    feat: ✨ Include waving hand

--- src/app/sites/home/components/hero/hero.component.ts
+++ src/app/sites/home/components/hero/hero.component.ts
@@ -24,6 +24,7 @@ import { NgOptimizedImage } from '@angular/common';
       simpleXing,
     }),
   ],
+  styleUrl: 'hero.component.scss',
 })
 export class HeroComponent {
   constructor(private readonly _store: StoreService) {}
