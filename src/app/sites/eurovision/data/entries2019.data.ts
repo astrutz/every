8d26ffe5c125e291e58a187594432f37c0d7345e
astrutz <alex-strutz@t-online.de>
2025-09-02 14:27:31 +0200

    feat: ✨ Add data for all entries

--- src/app/sites/eurovision/data/entries2019.data.ts
+++ src/app/sites/eurovision/data/entries2019.data.ts
@@ -1,6 +1,128 @@
 import { Entry } from '../dataobjects/entry.dataobject';
-import { estonia } from './countries.data';
+import {
+  albania,
+  armenia,
+  azerbaijan,
+  australia,
+  austria,
+  belgium,
+  croatia,
+  cyprus,
+  czechia,
+  denmark,
+  estonia,
+  finland,
+  france,
+  georgia,
+  germany,
+  greece,
+  iceland,
+  ireland,
+  israel,
+  italy,
+  latvia,
+  lithuania,
+  malta,
+  moldova,
+  netherlands,
+  northMacedonia,
+  norway,
+  poland,
+  portugal,
+  romania,
+  russia,
+  sanMarino,
+  serbia,
+  slovenia,
+  spain,
+  sweden,
+  switzerland,
+  ukraine,
+  unitedKingdom,
+  montenegro,
+  belarus,
+} from './countries.data';
 import { Rating } from '../dataobjects/rating.dataobject';
+
+export const al2019: Entry = {
+  country: albania,
+  year: 2019,
+  place: 17,
+  artist: 'Jonida Maliqi',
+  title: 'Ktheju tokës',
+  link: '',
+  rating: new Rating(4, 3, 2, 3, 5),
+};
+
+export const am2019: Entry = {
+  country: armenia,
+  year: 2019,
+  place: 37,
+  artist: 'Srbuk',
+  title: 'Walking Out',
+  link: '',
+  rating: new Rating(4, 6, 5, 4, 3),
+};
+
+export const az2019: Entry = {
+  country: azerbaijan,
+  year: 2019,
+  place: 8,
+  artist: 'Chingiz',
+  title: 'Truth',
+  link: '',
+  rating: new Rating(7, 7, 6, 7, 8),
+};
+
+export const au2019: Entry = {
+  country: australia,
+  year: 2019,
+  place: 9,
+  artist: 'Kate Miller-Heidke',
+  title: 'Zero Gravity',
+  link: '',
+  rating: new Rating(7, 10, 7, 9, 9),
+};
+
+export const by2019: Entry = {
+  country: belarus,
+  year: 2019,
+  place: 24,
+  artist: 'Zena',
+  title: 'Like It',
+  link: '',
+  rating: new Rating(8, 5, 7, 7, 4),
+};
+
+export const be2019: Entry = {
+  country: belgium,
+  year: 2019,
+  place: 31,
+  artist: 'Eliot',
+  title: 'Wake Up',
+  link: '',
+  rating: new Rating(7, 4, 8, 6, 3),
+};
+
+export const dk2019: Entry = {
+  country: denmark,
+  year: 2019,
+  place: 12,
+  artist: 'Leonora',
+  title: 'Love is forever',
+  link: '',
+  rating: new Rating(6, 7, 2, 7, 9),
+};
+
+export const de2019: Entry = {
+  country: germany,
+  year: 2019,
+  place: 25,
+  artist: 'S!STERS',
+  title: 'Sister',
+  link: '',
+  rating: new Rating(3, 2, 1, 2, 7),
+};
 
 export const ee2019: Entry = {
   country: estonia,
@@ -12,4 +134,366 @@ export const ee2019: Entry = {
   rating: new Rating(5, 2, 2, 3, 4),
 };
 
-export const entries2019: Entry[] = [ee2019];
+export const fi2019: Entry = {
+  country: finland,
+  year: 2019,
+  place: 39,
+  artist: 'Darude',
+  title: 'Look Away',
+  link: '',
+  rating: new Rating(5, 7, 7, 4, 3),
+};
+
+export const fr2019: Entry = {
+  country: france,
+  year: 2019,
+  place: 16,
+  artist: 'Bilal Hassani',
+  title: 'Roi',
+  link: '',
+  rating: new Rating(4, 5, 6, 6, 3),
+};
+
+export const ge2019: Entry = {
+  country: georgia,
+  year: 2019,
+  place: 33,
+  artist: 'Oto Nemsadze',
+  title: 'Keep on Going',
+  link: '',
+  rating: new Rating(2, 4, 2, 2, 4),
+};
+
+export const gr2019: Entry = {
+  country: greece,
+  year: 2019,
+  place: 21,
+  artist: 'Katerine Duska',
+  title: 'Better Love',
+  link: '',
+  rating: new Rating(4, 3, 4, 3, 8),
+};
+
+export const ie2019: Entry = {
+  country: ireland,
+  year: 2019,
+  place: 41,
+  artist: 'Sarah McTernan',
+  title: '22',
+  link: '',
+  rating: new Rating(5, 7, 5, 4, 4),
+};
+
+export const is2019: Entry = {
+  country: iceland,
+  year: 2019,
+  place: 10,
+  artist: 'Hatari',
+  title: 'Hatrið mun sigra',
+  link: '',
+  rating: new Rating(7, 7, 7, 10, 9),
+};
+
+export const il2019: Entry = {
+  country: israel,
+  year: 2019,
+  place: 23,
+  artist: 'Kobi Marimi',
+  title: 'Home',
+  link: '',
+  rating: new Rating(3, 5, 2, 1, 7),
+};
+
+export const it2019: Entry = {
+  country: italy,
+  year: 2019,
+  place: 2,
+  artist: 'Mahmood',
+  title: 'Soldi',
+  link: '',
+  rating: new Rating(8, 8, 9, 9, 5),
+};
+
+export const hr2019: Entry = {
+  country: croatia,
+  year: 2019,
+  place: 34,
+  artist: 'Roko',
+  title: 'The Dream',
+  link: '',
+  rating: new Rating(4, 6, 2, 4, 7),
+};
+
+export const lv2019: Entry = {
+  country: latvia,
+  year: 2019,
+  place: 36,
+  artist: 'Carousel',
+  title: 'That Night',
+  link: '',
+  rating: new Rating(6, 3, 4, 7, 8),
+};
+
+export const lt2019: Entry = {
+  country: lithuania,
+  year: 2019,
+  place: 29,
+  artist: 'Jurij Veklenko',
+  title: 'Run with the Lions',
+  link: '',
+  rating: new Rating(4, 3, 2, 2, 7),
+};
+
+export const mt2019: Entry = {
+  country: malta,
+  year: 2019,
+  place: 14,
+  artist: 'Michela',
+  title: 'Chameleon',
+  link: '',
+  rating: new Rating(7, 7, 8, 7, 3),
+};
+
+export const md2019: Entry = {
+  country: moldova,
+  year: 2019,
+  place: 30,
+  artist: 'Anna Odobescu',
+  title: 'Stay',
+  link: '',
+  rating: new Rating(5, 10, 4, 8, 7),
+};
+
+export const me2019: Entry = {
+  country: montenegro,
+  year: 2019,
+  place: 38,
+  artist: 'D MOL',
+  title: 'Heaven',
+  link: '',
+  rating: new Rating(3, 1, 1, 2, 4),
+};
+
+export const nl2019: Entry = {
+  country: netherlands,
+  year: 2019,
+  place: 1,
+  artist: 'Duncan Laurence',
+  title: 'Arcade',
+  link: '',
+  rating: new Rating(4, 4, 5, 2, 7),
+};
+
+export const mk2019: Entry = {
+  country: northMacedonia,
+  year: 2019,
+  place: 7,
+  artist: 'Tamara Todevska',
+  title: 'Proud',
+  link: '',
+  rating: new Rating(2, 2, 1, 1, 6),
+};
+
+export const no2019: Entry = {
+  country: norway,
+  year: 2019,
+  place: 6,
+  artist: 'KEiiNO',
+  title: 'Spirit in the Sky',
+  link: '',
+  rating: new Rating(10, 8, 9, 10, 8),
+};
+
+export const at2019: Entry = {
+  country: austria,
+  year: 2019,
+  place: 40,
+  artist: 'PÆNDA',
+  title: 'Limits',
+  link: '',
+  rating: new Rating(2, 3, 1, 2, 2),
+};
+
+export const pl2019: Entry = {
+  country: poland,
+  year: 2019,
+  place: 27,
+  artist: 'Tulia',
+  title: 'Fire of Love',
+  link: '',
+  rating: new Rating(4, 5, 3, 3, 2),
+};
+
+export const pt2019: Entry = {
+  country: portugal,
+  year: 2019,
+  place: 35,
+  artist: 'Conan Osiris',
+  title: 'Telemóveis',
+  link: '',
+  rating: new Rating(5, 4, 1, 3, 7),
+};
+
+export const ro2019: Entry = {
+  country: romania,
+  year: 2019,
+  place: 32,
+  artist: 'Ester Peony',
+  title: 'On a Sunday',
+  link: '',
+  rating: new Rating(6, 7, 6, 8, 8),
+};
+
+export const ru2019: Entry = {
+  country: russia,
+  year: 2019,
+  place: 3,
+  artist: 'Sergey Lazarev',
+  title: 'Scream',
+  link: '',
+  rating: new Rating(4, 6, 2, 3, 5),
+};
+
+export const sm2019: Entry = {
+  country: sanMarino,
+  year: 2019,
+  place: 19,
+  artist: 'Serhat',
+  title: 'Say Na Na Na',
+  link: '',
+  rating: new Rating(8, 8, 7, 9, 4),
+};
+
+export const se2019: Entry = {
+  country: sweden,
+  year: 2019,
+  place: 5,
+  artist: 'John Lundvik',
+  title: 'Too Late for Love',
+  link: '',
+  rating: new Rating(5, 4, 2, 3, 7),
+};
+
+export const ch2019: Entry = {
+  country: switzerland,
+  year: 2019,
+  place: 4,
+  artist: 'Luca Hänni',
+  title: 'She Got Me',
+  link: '',
+  rating: new Rating(9, 7, 10, 8, 7),
+};
+
+export const rs2019: Entry = {
+  country: serbia,
+  year: 2019,
+  place: 18,
+  artist: 'Nevena Božović',
+  title: 'Kruna',
+  link: '',
+  rating: new Rating(3, 2, 3, 2, 8),
+};
+
+export const si2019: Entry = {
+  country: slovenia,
+  year: 2019,
+  place: 15,
+  artist: 'Zala Kralj & Gašper Šantl',
+  title: 'Sebi',
+  link: '',
+  rating: new Rating(6, 2, 2, 4, 8),
+};
+
+export const es2019: Entry = {
+  country: spain,
+  year: 2019,
+  place: 22,
+  artist: 'Miki',
+  title: 'La Venda',
+  link: '',
+  rating: new Rating(9, 7, 10, 9, 4),
+};
+
+export const cz2019: Entry = {
+  country: czechia,
+  year: 2019,
+  place: 11,
+  artist: 'Lake Malawi',
+  title: 'Friend of a Friend',
+  link: '',
+  rating: new Rating(6, 7, 3, 7, 6),
+};
+
+export const ua2019: Entry = {
+  country: ukraine,
+  year: 2019,
+  place: 1,
+  artist: 'Go_A',
+  title: 'Shum',
+  link: '',
+  rating: new Rating(9, 7, 10, 10, 10),
+};
+
+export const uk2019: Entry = {
+  country: unitedKingdom,
+  year: 2019,
+  place: 26,
+  artist: 'Michael Rice',
+  title: 'Bigger Than Us',
+  link: '',
+  rating: new Rating(4, 1, 2, 2, 6),
+};
+
+export const cy2019: Entry = {
+  country: cyprus,
+  year: 2019,
+  place: 13,
+  artist: 'Tamta',
+  title: 'Replay',
+  link: '',
+  rating: new Rating(6, 6, 8, 6, 3),
+};
+
+export const entries2019: Entry[] = [
+  al2019,
+  am2019,
+  az2019,
+  au2019,
+  by2019,
+  be2019,
+  dk2019,
+  de2019,
+  ee2019,
+  fi2019,
+  fr2019,
+  ge2019,
+  gr2019,
+  ie2019,
+  is2019,
+  il2019,
+  it2019,
+  hr2019,
+  lv2019,
+  lt2019,
+  mt2019,
+  md2019,
+  me2019,
+  nl2019,
+  mk2019,
+  no2019,
+  at2019,
+  pl2019,
+  pt2019,
+  ro2019,
+  ru2019,
+  sm2019,
+  se2019,
+  ch2019,
+  rs2019,
+  si2019,
+  es2019,
+  cz2019,
+  ua2019,
+  uk2019,
+  cy2019,
+];
--- src/app/sites/eurovision/data/entries2022.data.ts
+++ src/app/sites/eurovision/data/entries2022.data.ts
@@ -1,6 +1,127 @@
 import { Entry } from '../dataobjects/entry.dataobject';
-import { estonia } from './countries.data';
+import {
+  albania,
+  armenia,
+  azerbaijan,
+  australia,
+  austria,
+  belgium,
+  bulgaria,
+  croatia,
+  cyprus,
+  czechia,
+  denmark,
+  estonia,
+  finland,
+  france,
+  georgia,
+  germany,
+  greece,
+  iceland,
+  ireland,
+  israel,
+  italy,
+  latvia,
+  lithuania,
+  malta,
+  moldova,
+  montenegro,
+  netherlands,
+  northMacedonia,
+  norway,
+  poland,
+  portugal,
+  romania,
+  sanMarino,
+  serbia,
+  slovenia,
+  spain,
+  sweden,
+  switzerland,
+  ukraine,
+  unitedKingdom,
+} from './countries.data';
 import { Rating } from '../dataobjects/rating.dataobject';
+
+export const al2022: Entry = {
+  country: albania,
+  year: 2022,
+  place: 28,
+  artist: 'Ronela Hajati',
+  title: 'Sekret',
+  link: '',
+  rating: new Rating(7, 3, 7, 9, 8),
+};
+
+export const am2022: Entry = {
+  country: armenia,
+  year: 2022,
+  place: 20,
+  artist: 'Rosa Linn',
+  title: 'Snap',
+  link: '',
+  rating: new Rating(7, 8, 6, 9, 8),
+};
+
+export const az2022: Entry = {
+  country: azerbaijan,
+  year: 2022,
+  place: 16,
+  artist: 'Nadir Rustamli',
+  title: 'Fade to Black',
+  link: '',
+  rating: new Rating(2, 5, 1, 3, 5),
+};
+
+export const au2022: Entry = {
+  country: australia,
+  year: 2022,
+  place: 15,
+  artist: 'Sheldon Riley',
+  title: 'Not the Same',
+  link: '',
+  rating: new Rating(1, 2, 0, 1, 4),
+};
+
+export const be2022: Entry = {
+  country: belgium,
+  year: 2022,
+  place: 19,
+  artist: 'Jeremie Makiese',
+  title: 'Miss You',
+  link: '',
+  rating: new Rating(4, 5, 4, 4, 3),
+};
+
+export const bg2022: Entry = {
+  country: bulgaria,
+  year: 2022,
+  place: 38,
+  artist: 'Intelligent Music Project',
+  title: 'Intention',
+  link: '',
+  rating: new Rating(5, 2, 2, 5, 3),
+};
+
+export const dk2022: Entry = {
+  country: denmark,
+  year: 2022,
+  place: 31,
+  artist: 'Reddi',
+  title: 'The Show',
+  link: '',
+  rating: new Rating(6, 4, 5, 7, 4),
+};
+
+export const de2022: Entry = {
+  country: germany,
+  year: 2022,
+  place: 25,
+  artist: 'Malik Harris',
+  title: 'Rockstars',
+  link: '',
+  rating: new Rating(4, 7, 3, 5, 6),
+};
 
 export const ee2022: Entry = {
   country: estonia,
@@ -12,4 +133,355 @@ export const ee2022: Entry = {
   rating: new Rating(6, 3, 5, 6, 6),
 };
 
-export const entries2022: Entry[] = [ee2022];
+export const fi2022: Entry = {
+  country: finland,
+  year: 2022,
+  place: 21,
+  artist: 'The Rasmus',
+  title: 'Jezebel',
+  link: '',
+  rating: new Rating(3, 5, 7, 5, 3),
+};
+
+export const fr2022: Entry = {
+  country: france,
+  year: 2022,
+  place: 24,
+  artist: 'Alvan & Ahez',
+  title: 'Fulenn',
+  link: '',
+  rating: new Rating(9, 3, 9, 7, 4),
+};
+
+export const ge2022: Entry = {
+  country: georgia,
+  year: 2022,
+  place: 39,
+  artist: 'Circus Mircus',
+  title: 'Lock Me In',
+  link: '',
+  rating: new Rating(3, 3, 1, 4, 3),
+};
+
+export const gr2022: Entry = {
+  country: greece,
+  year: 2022,
+  place: 8,
+  artist: 'Amanda Georgiadi Tenfjord',
+  title: 'Die Together',
+  link: '',
+  rating: new Rating(1, 2, 0, 2, 8),
+};
+
+export const ie2022: Entry = {
+  country: ireland,
+  year: 2022,
+  place: 34,
+  artist: 'Brooke',
+  title: "That's Rich",
+  link: '',
+  rating: new Rating(7, 5, 6, 7, 4),
+};
+
+export const is2022: Entry = {
+  country: iceland,
+  year: 2022,
+  place: 23,
+  artist: 'Systur',
+  title: 'Með hækkandi sól',
+  link: '',
+  rating: new Rating(3, 2, 2, 4, 3),
+};
+
+export const il2022: Entry = {
+  country: israel,
+  year: 2022,
+  place: 30,
+  artist: 'Michael Ben David',
+  title: 'I.M.',
+  link: '',
+  rating: new Rating(7, 6, 7, 5, 6),
+};
+
+export const it2022: Entry = {
+  country: italy,
+  year: 2022,
+  place: 6,
+  artist: 'Mahmood & Blanco',
+  title: 'Brividi',
+  link: '',
+  rating: new Rating(3, 1, 2, 1, 8),
+};
+
+export const hr2022: Entry = {
+  country: croatia,
+  year: 2022,
+  place: 26,
+  artist: 'Mia Dimšić',
+  title: 'Guilty Pleasure',
+  link: '',
+  rating: new Rating(7, 3, 4, 4, 7),
+};
+
+export const lv2022: Entry = {
+  country: latvia,
+  year: 2022,
+  place: 32,
+  artist: 'Citi Zēni',
+  title: 'Eat Your Salad',
+  link: '',
+  rating: new Rating(9, 6, 5, 8, 4),
+};
+
+export const lt2022: Entry = {
+  country: lithuania,
+  year: 2022,
+  place: 14,
+  artist: 'Monika Liu',
+  title: 'Sentimentai',
+  link: '',
+  rating: new Rating(8, 6, 8, 10, 8),
+};
+
+export const mt2022: Entry = {
+  country: malta,
+  year: 2022,
+  place: 35,
+  artist: 'Emma Muscat',
+  title: 'I Am What I Am',
+  link: '',
+  rating: new Rating(5, 6, 5, 6, 3),
+};
+
+export const md2022: Entry = {
+  country: moldova,
+  year: 2022,
+  place: 7,
+  artist: 'Zdob și Zdub & Advahov Brothers',
+  title: 'Trenulețul',
+  link: '',
+  rating: new Rating(10, 7, 8, 10, 5),
+};
+
+export const me2022: Entry = {
+  country: montenegro,
+  year: 2022,
+  place: 37,
+  artist: 'Vladana',
+  title: 'Breathe',
+  link: '',
+  rating: new Rating(3, 4, 1, 1, 8),
+};
+
+export const nl2022: Entry = {
+  country: netherlands,
+  year: 2022,
+  place: 11,
+  artist: 'S10',
+  title: 'De Diepte',
+  link: '',
+  rating: new Rating(8, 5, 9, 9, 9),
+};
+
+export const mk2022: Entry = {
+  country: northMacedonia,
+  year: 2022,
+  place: 27,
+  artist: 'Andrea',
+  title: 'Circles',
+  link: '',
+  rating: new Rating(4, 4, 2, 3, 5),
+};
+
+export const no2022: Entry = {
+  country: norway,
+  year: 2022,
+  place: 10,
+  artist: 'Subwoolfer',
+  title: 'Give That Wolf a Banana',
+  link: '',
+  rating: new Rating(7, 6, 3, 7, 6),
+};
+
+export const at2022: Entry = {
+  country: austria,
+  year: 2022,
+  place: 36,
+  artist: 'Lum!x feat. Pia Maria',
+  title: 'Halo',
+  link: '',
+  rating: new Rating(9, 6, 9, 9, 2),
+};
+
+export const pl2022: Entry = {
+  country: poland,
+  year: 2022,
+  place: 12,
+  artist: 'Ochman',
+  title: 'River',
+  link: '',
+  rating: new Rating(8, 5, 7, 9, 10),
+};
+
+export const pt2022: Entry = {
+  country: portugal,
+  year: 2022,
+  place: 9,
+  artist: 'Maro',
+  title: 'Saudade Saudade',
+  link: '',
+  rating: new Rating(4, 2, 4, 3, 6),
+};
+
+export const ro2022: Entry = {
+  country: romania,
+  year: 2022,
+  place: 18,
+  artist: 'WRS',
+  title: 'Llámame',
+  link: '',
+  rating: new Rating(10, 8, 6, 10, 6),
+};
+
+export const sm2022: Entry = {
+  country: sanMarino,
+  year: 2022,
+  place: 33,
+  artist: 'Achille Lauro',
+  title: 'Stripper',
+  link: '',
+  rating: new Rating(4, 3, 2, 3, 3),
+};
+
+export const se2022: Entry = {
+  country: sweden,
+  year: 2022,
+  place: 4,
+  artist: 'Cornelia Jakobs',
+  title: 'Hold Me Closer',
+  link: '',
+  rating: new Rating(5, 2, 7, 3, 8),
+};
+
+export const ch2022: Entry = {
+  country: switzerland,
+  year: 2022,
+  place: 17,
+  artist: 'Marius Bear',
+  title: 'Boys Do Cry',
+  link: '',
+  rating: new Rating(2, 3, 1, 2, 4),
+};
+
+export const rs2022: Entry = {
+  country: serbia,
+  year: 2022,
+  place: 5,
+  artist: 'Konstrakta',
+  title: 'In Corpore Sano',
+  link: '',
+  rating: new Rating(8, 8, 7, 10, 8),
+};
+
+export const si2022: Entry = {
+  country: slovenia,
+  year: 2022,
+  place: 40,
+  artist: 'LPS',
+  title: 'Disko',
+  link: '',
+  rating: new Rating(4, 2, 2, 3, 4),
+};
+
+export const es2022: Entry = {
+  country: spain,
+  year: 2022,
+  place: 3,
+  artist: 'Chanel',
+  title: 'SloMo',
+  link: '',
+  rating: new Rating(10, 10, 5, 9, 8),
+};
+
+export const cz2022: Entry = {
+  country: czechia,
+  year: 2022,
+  place: 22,
+  artist: 'We Are Domi',
+  title: 'Lights Off',
+  link: '',
+  rating: new Rating(10, 9, 8, 10, 6),
+};
+
+export const ua2022: Entry = {
+  country: ukraine,
+  year: 2022,
+  place: 1,
+  artist: 'Kalush Orchestra',
+  title: 'Stefania',
+  link: '',
+  rating: new Rating(8, 5, 9, 9, 6),
+};
+
+export const uk2022: Entry = {
+  country: unitedKingdom,
+  year: 2022,
+  place: 2,
+  artist: 'Sam Ryder',
+  title: 'Space Man',
+  link: '',
+  rating: new Rating(7, 7, 6, 7, 10),
+};
+
+export const cy2022: Entry = {
+  country: cyprus,
+  year: 2022,
+  place: 29,
+  artist: 'Andromache',
+  title: 'Ela',
+  link: '',
+  rating: new Rating(7, 6, 9, 7, 3),
+};
+
+export const entries2022: Entry[] = [
+  al2022,
+  am2022,
+  az2022,
+  au2022,
+  be2022,
+  bg2022,
+  dk2022,
+  de2022,
+  ee2022,
+  fi2022,
+  fr2022,
+  ge2022,
+  gr2022,
+  ie2022,
+  is2022,
+  il2022,
+  it2022,
+  hr2022,
+  lv2022,
+  lt2022,
+  mt2022,
+  md2022,
+  me2022,
+  nl2022,
+  mk2022,
+  no2022,
+  at2022,
+  pl2022,
+  pt2022,
+  ro2022,
+  sm2022,
+  se2022,
+  ch2022,
+  rs2022,
+  si2022,
+  es2022,
+  cz2022,
+  ua2022,
+  uk2022,
+  cy2022,
+];
