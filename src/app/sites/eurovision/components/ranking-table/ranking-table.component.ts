707c649fe62ba7fd7402dc2698aa9273cb4fc8d6
astrutz <alex-strutz@t-online.de>
2025-09-08 10:12:39 +0200

    fix: 🐛 Add colors for non on brand

--- src/app/sites/eurovision/components/ranking-table/ranking-table.component.ts
+++ src/app/sites/eurovision/components/ranking-table/ranking-table.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Contest } from '../../dataobjects/contest.dataobject';
 import { Country } from '../../dataobjects/country.dataobject';
 import { Entry } from '../../dataobjects/entry.dataobject';
 import { Util } from '../../services/util';
 import { RouterLink } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { DOCUMENT, NgClass } from '@angular/common';
 
 @Component({
   selector: 'eurovision-ranking-table',
@@ -13,17 +13,20 @@ import { NgClass } from '@angular/common';
   imports: [RouterLink, NgClass],
 })
 export class RankingTableComponent<T extends Contest | Country | Entry> {
+  private document = inject(DOCUMENT);
+
   @Input({ required: true })
   sortedEntities!: T[];
 
   @Input()
-  colorScheme: 'light' | 'dark' = 'light';
+  colorScheme?: 'light' | 'dark' = undefined;
 
   @Input()
   showFlag: boolean = true;
 
   protected getFlag(code: string): string {
-    return `${code}-${this.colorScheme}`;
+    const colorScheme = this.document.documentElement.classList.contains('dark') ? 'light' : 'dark';
+    return `${code}-${this.colorScheme ?? colorScheme}`;
   }
 
   protected readonly Util = Util;
