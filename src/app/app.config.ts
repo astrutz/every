fdc28986541029bf8b56ae3a135abc0a870bed9a
astrutz <alex-strutz@t-online.de>
2026-02-25 22:25:44 +0100

    fix: 🐛 Remove interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-import { ApiKeyInterceptor } from './interceptors/api-key.interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true },
     provideRouter(
       routes,
       withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'top' }),
