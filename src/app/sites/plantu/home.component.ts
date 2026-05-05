c2c28dfd8167b1fb742b799ddb5e9545902059ff
astrutz <alex-strutz@t-online.de>
2026-05-05 09:02:20 +0200

    feat: ✨ #104 Add plantu app

---
+++ src/app/sites/plantu/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ContentAreaComponent } from '../../components/content-area/content-area.component';
+
+@Component({
+  selector: 'plantu-home',
+  templateUrl: 'home.component.html',
+  imports: [ContentAreaComponent],
+})
+export class HomeComponent {}
